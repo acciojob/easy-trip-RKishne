f827d9d5117e3d78a3a1fe44c758cb34f78f17a0
Rahul Kishne <kishnerahul123@gmail.com>
2023-10-04 14:34:57 +0530

    8th

--- src/main/java/com/driver/repository/Repository.java
+++ src/main/java/com/driver/repository/Repository.java
@@ -48,7 +48,7 @@ public class Repository {
         List<Flight> ans=new ArrayList<>();
         for(Flight flight:fligthDb.values()) {
             if (flight.getFlightDate() == date && flight.getFromCity().equals(airportName)) {
-                ans.add(flight)
+                ans.add(flight);
             }
         }
 
